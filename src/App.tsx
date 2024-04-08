b74d15f9125458924f2619e016cd71ba6347efd3
Guilherme Souza <guilhermesouzae21@gmail.com>
2024-04-07 22:15:17 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Login } from "./Pages/Login";
+
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <Login />
+    </main>
+  );
+}
+
+export default App;
